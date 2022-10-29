921687402116529b9a4b378a0381ed93f8e77f8e
Amanda <amandarb43@gmail.com>
2022-10-29 14:43:26 -0700

    Solution to refresh, delay, and lost display

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 var lost = document.getElementById("Lost");
 
-
 function jump(){
    if(character.classList.add !="animate") {
     character.classList.add("animate") ;
@@ -20,17 +19,11 @@ var checkDead = setInterval (function () {
     if(blockLeft<20 && blockLeft>0 && characterTop>=130) {
             block.style.animation = "none" ;
             block.style.display = "none" ;
-            lost.style.display = "";
+            document.getElementById('lost').innerHTML = "You lose!";
         }
 },10);
-//Code below is a test that isnt working. No display to show win or loss
-/*function display(){
-if (checkDead() === true)   {
-        document.getElementById("Lost").style.display = 'block';
-        document.getElementById("Lost").style.display = 'none';
+
+//Code below is for restart button
+function restart(){
+      window.location.reload();
         }
-else    {
-        document.getElementById("Win").style.display ='block';
-        document.getElementById("Win").style.display ='none';
-        }
-}*/
